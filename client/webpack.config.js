ac47e2d0fc514e958f8e8f7d7f52bb0cee6eb1f7
NguyenHoaiPhuong <NguyenHoaiPhuong@github.com>
2019-09-23 00:56:21 +0700

    Convert copyright.js to tsx file

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -19,10 +19,20 @@ module.exports = {
         poll: 1000,
         ignored: /node_modules/
     },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.json']
+    },
     module: {
         rules: [
             {
                 test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.tsx$/,
                 exclude: /node_modules/,
                 use: {
                     loader: 'babel-loader'
