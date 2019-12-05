fe5a81fbb9f4675ecacbb7a36717cbd75ee9eba9
Nguyen Hoai Phuong <phuong.nguyen@cel-consulting.com>
2019-12-05 09:38:04 +0700

    [Client] Clean

--- client/src/reducers/index.ts
+++
@@ -1,9 +0,0 @@
-import {combineReducers} from 'redux';
-import {getUser, setUser} from './user'
-
-const AllReducers = combineReducers({
-    user: getUser,
-    setUser: setUser
-})
-
-export default AllReducers;
